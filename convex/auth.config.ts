a99e7c3bf932d67b4acc64176f10daa3ac17df91
Shruhath <shruhathkreddy@gmail.com>
2026-02-14 22:37:06 +0530

    intial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://securetoken.google.com/finura-ea4bc",
+      applicationID: "finura-ea4bc",
+    },
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="loading-screen">
+      <div className="loading-logo">
+        <span className="loading-logo__text">Fin</span>
+        <span className="loading-logo__hash">#</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
