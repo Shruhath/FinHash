a8b246c90873ec592aa76c38dbdbdd56745de33b
Shruhath <shruhathkreddy@gmail.com>
2026-03-01 10:20:30 +0530

    changed configs

---
+++ convex/_generated/auth.config.ts
@@ -0,0 +1,10 @@
+export default {
+  providers: [
+    {
+      // The domain where tokens are issued (Firebase)
+      domain: "https://securetoken.google.com/finura-ea4bc", 
+      // The "Audience" (Your Firebase Project ID)
+      applicationID: "finura-ea4bc",
+    },
+  ],
+};
